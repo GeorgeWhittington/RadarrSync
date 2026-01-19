a759d8c5b71d70bf3e93e3fdc330fb00c4060331
George Whittington <80219357+GeorgeWhittington@users.noreply.github.com>
2026-01-19 23:50:12 +0000

    Refactor RadarrSync.py

--- README.md
+++ README.md
@@ -1,42 +1,53 @@
 # RadarrSync
-Syncs two Radarr servers through web API.  
+
+Syncs two Radarr servers through web API.
 
 ### Why
+
 Many Plex servers choke if you try to transcode 4K files. To address this a common approach is to keep a 4k and a 1080/720 version in seperate libraries.
 
-Radarr does not support saving files to different folder roots for different quality profiles.  To save 4K files to a seperate library in plex you must run two Radarr servers.  This script looks for movies with a quality setting of 4k on one server and creates the movies on a second server.  
-
+Radarr does not support saving files to different folder roots for different quality profiles.  To save 4K files to a seperate library in plex you must run two Radarr servers.  This script looks for movies with a quality setting of 4k on one server and creates the movies on a second server.
 
 ### Configuration
- 1. Edit the Config.txt file and enter your servers URLs and API keys for each server.  
 
-    Example Config.txt:
-    ```ini
-    [Radarr]
-    url = https://example.com:443
-    key = FCKGW-RHQQ2-YXRKT-8TG6W-2B7Q8
-    
-    [Radarr4k]
-    url = http://127.0.0.1:8080
-    key = FCKGW-RHQQ2-YXRKT-8TG6W-2B7Q8
-    ```
- 2. Edit 4K profile on the server that will download 1080/720p files.  You want the quality profile to download the highest non-4k quality your Plex server can stream with choking. 
+1. Edit the Config.txt file and enter your servers URLs and API keys for each server.
 
+   Example Config.txt:
+
+   ```ini
+   [Radarr]
+   url = https://example.com:443
+   api_key = FCKGW-RHQQ2-YXRKT-8TG6W-2B7Q8
+
+   [Radarr4k]
+   url = http://127.0.0.1:8080
+   api_key = FCKGW-RHQQ2-YXRKT-8TG6W-2B7Q8
+   path_from = "/4K Movies/"
+   path_to = "/Movies/"
+   source_profile = 5
+   target_profile = 4
+   ```
+2. Edit 4K profile on the server that will download 1080/720p files.  You want the quality profile to download the highest non-4k quality your Plex server can stream with choking.
 
 #### How to Run
-Recomended to run using cron every 15 minutes or an interval of your preference.
+
+Create a python venv and install requirements.txt into it. Then set up a cron job on the interval of your choice to run the script RadarrSync.py using the python executable in that virtual environment. The script has the following arguments/parameters:
+
+- --config_file - The absolute or relative path to the config file you wish to use (Config.txt)
+- --source_section - The section in the config file which contains the movies you want to sync to all other sections
+  - (eg: if you set --source_section=Radarr, then any movies that exist in "Radarr" would be copied across to any/all of the other radarr instances you define in your config file)
+- --verbose - OPTIONAL, enables the debug log level
+
 ```bash
-python RadarSync.py
+python3 RadarSync.py Config.txt --source_section Radarr
 ```
 
-
 #### Requirements
- * Python 3.4 or greater
- * 2x Radarr servers
- * Install requirements.txt
+
+* Python 3.4 or greater
+* 2x Radarr servers
+* Install requirements.txt
 
 #### Notes
- * Ensure that the root path is the same on both servers. ie /movies
- 
- 
- 
+
+* Ensure that the root path is the same on both servers. ie /movies
--- RadarrSync.py
+++ RadarrSync.py
@@ -1,135 +1,115 @@
-#!/usr/bin/env python
-import os
 import logging
 import requests
 import json
 import configparser
-import sys
+import argparse
+from typing import Any
 
+API_PREFIX = "/api/v3/"
 
-DEV = False
-VER = '1.0.1'
+class RadarrInstance:
+    def __init__(self, name, config_section, source: bool):
+        self.name = name
+        self.url = config_section["url"]
+        self.api_key = config_section["api_key"]
 
-########################################################################################################################
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+        if not source:
+            self.source_profile = int(config_section["source_profile"])
+            self.target_profile = int(config_section["target_profile"])
+            self.path_from = config_section["path_from"]
+            self.path_to = config_section["path_to"]
 
-fileHandler = logging.FileHandler("./Output.txt")
-fileHandler.setFormatter(logFormatter)
-logger.addHandler(fileHandler)
+logger = logging.getLogger(__name__)
 
-consoleHandler = logging.StreamHandler(sys.stdout)
-consoleHandler.setFormatter(logFormatter)
-logger.addHandler(consoleHandler)
-########################################################################################################################
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="RadarrSync.py",
+        description="""Simple script that syncs all movies detected on one
+        radarr instance to another""")
+    parser.add_argument("--config_file", required=True)
+    parser.add_argument("--source_section", required=True)
+    parser.add_argument("--verbose", "-v", action="store_true")
+    return parser.parse_args()
 
+def read_config(config_file: str):
+    config = configparser.ConfigParser()
+    resp = config.read(config_file)
+    if len(resp) < 0:
+        raise SystemExit(f"Error: could not load the config file '{config_file}'")
+    return config
 
-def ConfigSectionMap(section):
-    dict1 = {}
-    options = Config.options(section)
-    for option in options:
+def get_config_value(config: configparser.ConfigParser, section: str, key: str) -> Any:
+    try:
+        return config[section][key]
+    except:
+        logger.warning(f"Could not fetch the option {section}.{key}, defaulting to None")
+        return None
+
+def get_movies(radarr_instance: RadarrInstance, session: requests.Session) -> dict:
+    resp = session.get(f"{radarr_instance.url}{API_PREFIX}movie?apikey={radarr_instance.api_key}")
+    resp.raise_for_status()
+    return resp.json()
+
+def sync_movies_to_target(radarr_target: RadarrInstance, source_movies: list, session: requests.Session):
+    """Iterates all movies in source_movies and imports them to the radarr_target if they:
+    - Have the profile id that the current radarr target specifies in source_profile
+    - Are not already present in the radarr target's library"""
+    target_movies = get_movies(radarr_target, session)
+    target_movie_ids = [movie["tmdbId"] for movie in target_movies]
+
+    for source_movie in source_movies:
+        if source_movie["qualityProfileId"] != radarr_target.source_profile:
+            logging.debug(f"Skipping {source_movie['title']}, wanted quality profile {radarr_target.source_profile} found profile {source_movie['profileId']}")
+            continue
+
+        if source_movie["tmdbId"] in target_movie_ids:
+            logging.debug(f"{source_movie['title']} already in {radarr_target.name} library")
+            continue
+
+        # New movie! Sync it across
+        payload = {
+            "title": source_movie["title"],
+            "qualityProfileId": radarr_target.target_profile,
+            "titleSlug": source_movie["titleSlug"],
+            "tmdbId": source_movie["tmdbId"],
+            "monitored": source_movie["monitored"],
+            "path": source_movie["path"].replace(
+                radarr_target.path_from, radarr_target.path_to),
+            "minimumAvailability": "released",
+            "addOptions": {"searchForMovie": True}
+            # images???
+        }
+        logger.debug(payload)
+
+        resp = session.post(
+            f"{radarr_target.url}{API_PREFIX}movie?apikey={radarr_target.api_key}",
+            data=json.dumps(payload),
+            headers={'Content-Type': 'application/json'})
         try:
-            dict1[option] = Config.get(section, option)
-            if dict1[option] == -1:
-                logger.debug("skip: %s" % option)
-        except:
-            print("exception on %s!" % option)
-            dict1[option] = None
-    return dict1
+            resp.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print(resp.content)
+            raise e
 
-logger.debug('RadarSync Version {}'.format(VER))
-Config = configparser.ConfigParser()
+        logger.info(f"Added movie {source_movie['title']} to {radarr_target.name} server")
 
-# Loads an alternate config file so that I can work on my servers without uploading config to github
-if DEV:
-    settingsFilename = os.path.join(os.getcwd(), 'Dev'
-                                                 'Config.txt')
-else:
-    settingsFilename = os.path.join(os.getcwd(), 'Config.txt')
-Config.read(settingsFilename)
+def main():
+    args = parse_args()
+    config = read_config(args.config_file)
 
-radarr_url = ConfigSectionMap("Radarr")['url']
-radarr_key = ConfigSectionMap("Radarr")['key']
-radarrSession = requests.Session()
-radarrSession.trust_env = False
-radarrMovies = radarrSession.get('{0}/api/movie?apikey={1}'.format(radarr_url, radarr_key))
-if radarrMovies.status_code != 200:
-    logger.error('Radarr server error - response {}'.format(radarrMovies.status_code))
-    sys.exit(0)
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
 
-for server in Config.sections():
+    radarr_source = RadarrInstance(args.source_section, config[args.source_section], True)
+    radarr_targets = [RadarrInstance(section, config[section], False) for section in config.sections() if section != args.source_section]
 
-    if server == 'Default' or server == "Radarr":
-        continue  # Default section handled previously as it always needed
+    session = requests.Session()
+    session.trust_env = False  # Disables proxies (Is this needed???)
 
-    else:
-        logger.debug('syncing to {0}'.format(server))
+    source_movies = get_movies(radarr_source, session)
 
-        session = requests.Session()
-        session.trust_env = False
-        SyncServer_url = ConfigSectionMap(server)['url']
-        SyncServer_key = ConfigSectionMap(server)['key']
-        SyncServer_target_profile = ConfigSectionMap(server)['target_profile']
-        SyncServerMovies = session.get('{0}/api/movie?apikey={1}'.format(SyncServer_url, SyncServer_key))
-        if SyncServerMovies.status_code != 200:
-            logger.error('4K Radarr server error - response {}'.format(SyncServerMovies.status_code))
-            sys.exit(0)
+    for radarr_target in radarr_targets:
+        logger.debug(f"Syncing movies to target: {radarr_target.name} (url={radarr_target.url})")
+        sync_movies_to_target(radarr_target, source_movies, session)
 
-    # build a list of movied IDs already in the sync server, this is used later to prevent readding a movie that already
-    # exists.
-    # TODO refactor variable names to make it clear this builds list of existing not list of movies to add
-    # TODO #11 add reconcilliation to remove movies that have been deleted from source server
-    movieIds_to_syncserver = []
-    for movie_to_sync in SyncServerMovies.json():
-        movieIds_to_syncserver.append(movie_to_sync['tmdbId'])
-        #logger.debug('found movie to be added')
-
-    newMovies = 0
-    searchid = []
-    for movie in radarrMovies.json():
-        if movie['profileId'] == int(ConfigSectionMap(server)['profile']):
-            if movie['tmdbId'] not in movieIds_to_syncserver:
-                logging.debug('title: {0}'.format(movie['title']))
-                logging.debug('qualityProfileId: {0}'.format(movie['qualityProfileId']))
-                logging.debug('titleSlug: {0}'.format(movie['titleSlug']))
-                images = movie['images']
-                for image in images:
-                    image['url'] = '{0}{1}'.format(radarr_url, image['url'])
-                    logging.debug(image['url'])
-                logging.debug('tmdbId: {0}'.format(movie['tmdbId']))
-                logging.debug('path: {0}'.format(movie['path']))
-                logging.debug('monitored: {0}'.format(movie['monitored']))
-
-                # Update the path based on "path_from" and "path_to" passed to us in Config.txt
-                path = movie['path']
-                path = path.replace(ConfigSectionMap(server)['path_from'], ConfigSectionMap(server)['path_to'])
-
-                payload = {'title': movie['title'],
-                           'qualityProfileId': movie['qualityProfileId'],
-                           'titleSlug': movie['titleSlug'],
-                           'tmdbId': movie['tmdbId'],
-                           'path': path,
-                           'monitored': movie['monitored'],
-                           'images': images,
-                           'profileId': SyncServer_target_profile, 
-                           'minimumAvailability': 'released'
-                           }
-
-                r = session.post('{0}/api/movie?apikey={1}'.format(SyncServer_url, SyncServer_key), data=json.dumps(payload))
-                searchid.append(int(r.json()['id']))
-                logger.info('adding {0} to {1} server'.format(movie['title'], server))
-            else:
-                logging.debug('{0} already in {1} library'.format(movie['title'], server))
-        else:
-            logging.debug('Skipping {0}, wanted profile: {1} found profile: {2}'.format(movie['title'],
-                                                                                        movie['profileId'],
-                                                                                        int(ConfigSectionMap(server)['profile'])
-                                                                                        ))
-
-
-
-    if len(searchid):
-        payload = {'name' : 'MoviesSearch', 'movieIds' : searchid}
-        session.post('{0}/api/command?apikey={1}'.format(SyncServer_url, SyncServer_key), data=json.dumps(payload))
-
+if __name__ == "__main__":
+    main()
